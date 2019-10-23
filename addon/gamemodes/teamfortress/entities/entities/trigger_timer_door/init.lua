843b3d09d0fdd934499e44404c2c78434f9e4227
KrazyLittleSoldier <seamusmario555@gmail.com>
2019-10-22 22:08:50 -0400

    update timer_gate_door

--- addon/gamemodes/teamfortress/entities/entities/trigger_timer_door/init.lua
+++ addon/gamemodes/teamfortress/entities/entities/trigger_timer_door/init.lua
@@ -49,8 +49,6 @@ function ENT:StartTouch(ent)
 			end)
 			for k,v in pairs(team.GetPlayers(2)) do
 				if v:GetNWBool("Taunting") == true then return end
-				if not v:IsOnGround() then return end
-				if v:WaterLevel() ~= 0 then return end
 				timer.Create("StunRobot100", 0.001, 1, function()
 					v:DoAnimationEvent(ACT_MP_STUN_BEGIN)
 					timer.Create("StunRobotloop103", 0.6, 0, function()
@@ -61,6 +59,7 @@ function ENT:StartTouch(ent)
 				end)
 				v:SetNWBool("Taunting", true)
 				v:SetNWBool("NoWeapon", true)
+				v:Freeze(true)
 				net.Start("ActivateTauntCam")
 				net.Send(v)
 				timer.Simple(21.519, function()
@@ -68,7 +67,7 @@ function ENT:StartTouch(ent)
 					timer.Stop("StunRobotloop103")
 					timer.Stop("StunRobotloop104")
 					v:DoAnimationEvent(ACT_MP_STUN_END)
-					usermessage.Start("DeActivateTauntCam")
+					net.Start("DeActivateTauntCam")
 					net.Send(v)
 					v:Freeze(false)
 					v:SetNWBool("NoWeapon", false)
@@ -131,6 +130,38 @@ function ENT:StartTouch(ent)
 				for _,alarm in pairs (ents.FindByName("gate2_alarm_yellow_flash")) do
 					alarm:Fire("Stop")
 				end
+				ent:EmitSound("mvm/mvm_robo_stun.wav", 0, 100)
+	
+				timer.Simple(21.519, function()
+					ent:EmitSound("misc/cp_harbor_red_whistle.wav", 0, 100)
+				end)
+				for k,v in pairs(team.GetPlayers(2)) do
+					if v:GetNWBool("Taunting") == true then return end
+					timer.Create("StunRobot100", 0.001, 1, function()
+						v:DoAnimationEvent(ACT_MP_STUN_BEGIN)
+						timer.Create("StunRobotloop103", 0.6, 0, function()
+							timer.Create("StunRobotloop104", 0.22, 0, function()
+								v:DoAnimationEvent(ACT_MP_STUN_MIDDLE)
+							end)
+						end)
+					end)
+					v:SetNWBool("Taunting", true)
+					v:SetNWBool("NoWeapon", true)
+					v:Freeze(true)
+					net.Start("ActivateTauntCam")
+					net.Send(v)
+					timer.Simple(21.519, function()
+						if not IsValid(v) or (not v:Alive() and not v:GetNWBool("Taunting")) then v:Freeze(false) return end
+						timer.Stop("StunRobotloop103")
+						timer.Stop("StunRobotloop104")
+						v:DoAnimationEvent(ACT_MP_STUN_END)
+						net.Start("DeActivateTauntCam")
+						net.Send(v)
+						v:Freeze(false)
+						v:SetNWBool("NoWeapon", false)
+						v:SetNWBool("Taunting", false)
+					end)
+				end
 				for _,light in pairs (ents.FindByName("gate2_emergency_light")) do
 					light:Fire("Skin", "1")
 					light:Fire("SetAnimation", "idle")
@@ -187,7 +218,7 @@ function ENT:EndTouch(ent)
 		if ent:Team() == TEAM_BLU then
 		if self:GetName() == "gate1_door_trigger" then
 			for k,v in pairs(ents.FindByName("gate1_spawn_door")) do
-				if self.FullyOpenGate1 != false then return end
+				if self.FullyOpenGate1 == true then return end
 				timer.Stop("CloseGate0Door")
 				v:Fire("SetSpeed", "15")
 				v:Fire("Close")
@@ -202,7 +233,7 @@ function ENT:EndTouch(ent)
 		end
 		if self:GetName() == "gate2_door_trigger" then
 			for k,v in pairs(ents.FindByName("gate2_spawn_door")) do
-				if self.FullyOpenGate2 != false then return end
+				if self.FullyOpenGate2 == true then return end
 				timer.Stop("CloseGate1Door")
 				timer.Stop("OpenGate4Door")
 				v:Fire("SetSpeed", "25")
